6e090fb98dbe1875ebc115096eb1114af6aba8e5
vanessamineiro <mj.vanessa@hotmail.com>
2026-02-16 16:39:58 -0300

    Icone de remover

--- scripts.js
+++ scripts.js
@@ -80,8 +80,14 @@ function expenseAdd(newExpense) {
         expenseAmount.classList.add("expense-amount")
         expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$","")}`
 
+        //Cria o ícone de remover
+        const removeIcon = document.createElement("img")
+        removeIcon.classList.add("remove-icon")
+        removeIcon.setAttribute("src","img/remove.svg")
+        removeIcon.setAttribute("alt","Remover")
+
         //Adiciona as informações no item
-        expenseItem.append(expenseIcon,expenseInfo, expenseAmount)
+        expenseItem.append(expenseIcon,expenseInfo, expenseAmount, removeIcon)
         expenseList.append(expenseItem)
     } catch (error) {
         alert("Não foi possível atualizar a lista de despesas.")
